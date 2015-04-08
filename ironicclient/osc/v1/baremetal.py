463740eac1a3ccfd7cd79841d21a67967325e85d
Brad P. Crochet <brad@redhat.com>
2015-04-08 11:39:06 -0400

    Implement the rest of the args for baremetal list
    Change-Id: I6ff9c12bd31f2c39bd5c704bd6d22a59c3719874

--- ironicclient/osc/v1/baremetal.py
+++ ironicclient/osc/v1/baremetal.py
@@ -18,6 +18,11 @@
 import logging
 
 from cliff import lister
+from openstackclient.common import utils as oscutils
+
+from ironicclient.common.i18n import _
+from ironicclient import exc
+from ironicclient.v1 import resource_fields as res_fields
 
 
 class ListBaremetal(lister.Lister):
@@ -25,22 +30,93 @@ class ListBaremetal(lister.Lister):
 
     log = logging.getLogger(__name__ + ".ListBaremetal")
 
+    def get_parser(self, prog_name):
+        parser = super(ListBaremetal, self).get_parser(prog_name)
+        parser.add_argument(
+            "--matching",
+            metavar="<hostname>",
+            help="Filter hypervisors using <hostname> substring",
+        )
+        parser.add_argument(
+            '--limit',
+            metavar='<limit>',
+            type=int,
+            help='Maximum number of nodes to return per request, '
+                 '0 for no limit. Default is the maximum number used '
+                 'by the Baremetal API Service.'
+        )
+        parser.add_argument(
+            '--marker',
+            metavar='<node>',
+            help='Node UUID (for example, of the last node in the list from '
+                 'a previous request). Returns the list of nodes after this '
+                 'UUID.'
+        )
+        parser.add_argument(
+            '--sort',
+            metavar="<key>[:<direction>]",
+            help='Sort output by selected keys and directions(asc or desc) '
+                 '(default: asc), multiple keys and directions can be '
+                 'specified separated by comma',
+        )
+        parser.add_argument(
+            '--maintenance',
+            dest='maintenance',
+            action='store_true',
+            default=False,
+            help="List nodes in maintenance mode.",
+        )
+        parser.add_argument(
+            '--associated',
+            dest='associated',
+            action='store_true',
+            default=False,
+            help="List nodes by instance association."
+        )
+        parser.add_argument(
+            '--detail',
+            dest='detail',
+            action='store_true',
+            default=False,
+            help="Show detailed information about the nodes."
+        )
+        parser.add_argument(
+            '--long',
+            dest='detail',
+            action='store_true',
+            default=False,
+            help="Show detailed information about the nodes. "
+                 "Alias for --detail.",
+        )
+        return parser
+
     def take_action(self, parsed_args):
         self.log.debug("take_action(%s)" % parsed_args)
         client = self.app.client_manager.baremetal
 
-        columns = (
-            "UUID",
-            "Instance UUID",
-            "Power State",
-            "Provisioning State",
-            "Maintenance"
-        )
-        data = client.node.list()
+        columns = tuple(res_fields.NODE_LIST_FIELD_LABELS)
 
-        return (
-            columns,
-            list((node.uuid, node.instance_uuid, node.power_state,
-                  node.provision_state, node.maintenance)
-                 for node in data)
-        )
+        params = {}
+        if parsed_args.limit is not None:
+            if parsed_args.limit < 0:
+                raise exc.CommandError(
+                    _('Expected non-negative --limit, got %s') %
+                    parsed_args.limit)
+            params['limit'] = parsed_args.limit
+        params['marker'] = parsed_args.marker
+        params['associated'] = parsed_args.associated
+        params['maintenance'] = parsed_args.maintenance
+
+        if parsed_args.detail:
+            columns = tuple(res_fields.NODE_FIELD_LABELS)
+        params['detail'] = parsed_args.detail
+
+        data = client.node.list(**params)
+
+        data = oscutils.sort_items(data, parsed_args.sort)
+
+        column_headers = columns
+
+        return (column_headers,
+                (oscutils.get_item_properties(s, columns, formatters={
+                    'Properties': oscutils.format_dict},) for s in data))
